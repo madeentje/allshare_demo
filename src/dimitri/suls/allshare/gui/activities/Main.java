ad5d1013320b8e9d06ed2f9f758b492651847fa7
madeentje <dimitri.suls@gmail.com>
2013-03-14 16:25:13 +0000

    Refactored a lot. Implemented ListView for each device-type. Implemented Observer for each device-type via anonymous classes instead of implementing the Observer on the Activity itself.

--- src/dimitri/suls/allshare/gui/activities/Main.java
+++ src/dimitri/suls/allshare/gui/activities/Main.java
@@ -23,6 +23,7 @@ import com.sec.android.allshare.control.TVController;
 import com.sec.android.allshare.control.TVController.RemoteKey;
 import com.sec.android.allshare.media.AVPlayer;
 import com.sec.android.allshare.media.ContentInfo;
+import com.sec.android.allshare.media.ImageViewer;
 
 import dimitri.suls.allshare.R;
 import dimitri.suls.allshare.control.tv.TVTouchListener;
@@ -35,15 +36,18 @@ import dimitri.suls.allshare.managers.serviceprovider.ServiceProviderManager;
 import dimitri.suls.allshare.managers.serviceprovider.ServiceProviderObserver;
 import dimitri.suls.allshare.media.MediaFinder;
 
-public class Main extends Activity implements ServiceProviderObserver, DeviceObserver<TVController> {
+public class Main extends Activity {
 	private ServiceProviderManager serviceProviderManager = null;
-	private DeviceManager<TVController> tvDeviceManager = null;
+	private DeviceManager<TVController> tvControllerDeviceManager = null;
 	private DeviceManager<AVPlayer> avPlayerDeviceManager = null;
+	private DeviceManager<ImageViewer> imageViewerDeviceManager = null;
 	private MediaFinder mediaFinder = null;
 	private TabHost tabHostMain = null;
 	private View tabRemote = null;
 	private View tabMedia = null;
-	private ListView listViewDevices = null;
+	private ListView listViewTVControllers = null;
+	private ListView listViewAVPlayers = null;
+	private ListView listViewImageViewers = null;
 	private TextView textViewSelectedDevice = null;
 	private EditText editTextBrowseTerm = null;
 	private ListView listViewSongs = null;
@@ -59,7 +63,112 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 
 		try {
 			serviceProviderManager = new ServiceProviderManager(this);
-			serviceProviderManager.addObserver(this);
+			serviceProviderManager.addObserver(new ServiceProviderObserver() {
+				@Override
+				public void createdServiceProvider() {
+					tvControllerDeviceManager = new DeviceManager<TVController>(DeviceType.DEVICE_TV_CONTROLLER, serviceProviderManager);
+					tvControllerDeviceManager.addObserver(new DeviceObserver<TVController>() {
+						@Override
+						public void changedSelectedDevice(TVController selectedDevice) {
+							if (selectedDevice == null) {
+								textViewSelectedDevice.setText("No TV-controller selected.");
+
+								tabHostMain.setCurrentTab(0);
+
+								tabRemote.setEnabled(false);
+							} else {
+								textViewSelectedDevice.setText("Selected TV-controller: " + selectedDevice.getName());
+
+								tabRemote.setEnabled(true);
+							}
+						}
+
+						@Override
+						public void addedDevice(TVController device) {
+							DeviceAdapter arrayAdapter = (DeviceAdapter) listViewTVControllers.getAdapter();
+
+							arrayAdapter.add(device);
+						}
+
+						@Override
+						public void removedDevice(TVController device) {
+							DeviceAdapter arrayAdapter = (DeviceAdapter) listViewTVControllers.getAdapter();
+
+							arrayAdapter.remove(device);
+						}
+					});
+
+					avPlayerDeviceManager = new DeviceManager<AVPlayer>(DeviceType.DEVICE_AVPLAYER, serviceProviderManager);
+					avPlayerDeviceManager.addObserver(new DeviceObserver<AVPlayer>() {
+						@Override
+						public void changedSelectedDevice(AVPlayer selectedDevice) {
+							if (selectedDevice == null) {
+								// TODO: Add textView for AV-players
+								textViewSelectedDevice.setText("No AV-player selected.");
+
+								tabHostMain.setCurrentTab(0);
+
+								tabMedia.setEnabled(false);
+							} else {
+								textViewSelectedDevice.setText("Selected AV-player: " + selectedDevice.getName());
+
+								tabMedia.setEnabled(true);
+							}
+						}
+
+						@Override
+						public void addedDevice(AVPlayer device) {
+							DeviceAdapter arrayAdapter = (DeviceAdapter) listViewAVPlayers.getAdapter();
+
+							arrayAdapter.add(device);
+						}
+
+						@Override
+						public void removedDevice(AVPlayer device) {
+							DeviceAdapter arrayAdapter = (DeviceAdapter) listViewAVPlayers.getAdapter();
+
+							arrayAdapter.remove(device);
+						}
+					});
+
+					imageViewerDeviceManager = new DeviceManager<ImageViewer>(DeviceType.DEVICE_IMAGEVIEWER, serviceProviderManager);
+					imageViewerDeviceManager.addObserver(new DeviceObserver<ImageViewer>() {
+						@Override
+						public void changedSelectedDevice(ImageViewer selectedDevice) {
+							if (selectedDevice == null) {
+								// TODO: Add textView for image-viewers
+								textViewSelectedDevice.setText("No image-viewer selected.");
+
+								tabHostMain.setCurrentTab(0);
+
+								// TODO: Finish up image-viewer functionality
+							} else {
+								textViewSelectedDevice.setText("Selected image-viewer: " + selectedDevice.getName());
+
+								// TODO: Finish up image-viewer functionality
+							}
+						}
+
+						@Override
+						public void addedDevice(ImageViewer device) {
+							DeviceAdapter arrayAdapter = (DeviceAdapter) listViewImageViewers.getAdapter();
+
+							arrayAdapter.add(device);
+						}
+
+						@Override
+						public void removedDevice(ImageViewer device) {
+							DeviceAdapter arrayAdapter = (DeviceAdapter) listViewImageViewers.getAdapter();
+
+							arrayAdapter.remove(device);
+						}
+					});
+
+					initializeTabTouch();
+
+					refreshDeviceList();
+				}
+			});
 		} catch (Exception exception) {
 			final Activity activityMain = this;
 			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
@@ -88,7 +197,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 		initializeTabTouch();
 		initializeTabHostMain();
 		initializeTabHostRemote();
-		initializeListViewDevices();
+		initializeEachDeviceListViews();
 		initializeTextViewSelectedDevice();
 		initializeEditTextBrowseTerm();
 		initializeListViewSongs();
@@ -145,18 +254,37 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	private void initializeTabTouch() {
 		View tabTouch = findViewById(R.id.tabTouch);
 
-		tabTouch.setOnTouchListener(new TVTouchListener(tvDeviceManager));
+		tabTouch.setOnTouchListener(new TVTouchListener(tvControllerDeviceManager));
 	}
 
-	private void initializeListViewDevices() {
-		listViewDevices = (ListView) findViewById(R.id.listViewDevices);
-
-		listViewDevices.setOnItemClickListener(new OnItemClickListener() {
+	private void initializeEachDeviceListViews() {
+		listViewTVControllers = (ListView) findViewById(R.id.listViewTVControllers);
+		listViewTVControllers.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
 				TVController selectedDevice = (TVController) adapterView.getItemAtPosition(position);
 
-				tvDeviceManager.setSelectedDevice(selectedDevice);
+				tvControllerDeviceManager.setSelectedDevice(selectedDevice);
+			}
+		});
+
+		listViewAVPlayers = (ListView) findViewById(R.id.listViewAVPlayers);
+		listViewAVPlayers.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+				AVPlayer selectedDevice = (AVPlayer) adapterView.getItemAtPosition(position);
+
+				avPlayerDeviceManager.setSelectedDevice(selectedDevice);
+			}
+		});
+
+		listViewImageViewers = (ListView) findViewById(R.id.listViewImageViewers);
+		listViewImageViewers.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+				ImageViewer selectedDevice = (ImageViewer) adapterView.getItemAtPosition(position);
+
+				imageViewerDeviceManager.setSelectedDevice(selectedDevice);
 			}
 		});
 	}
@@ -179,11 +307,6 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 				final Item selectedSong = (Item) adapterView.getItemAtPosition(position);
 				final ContentInfo contentInfo = new ContentInfo.Builder().build();
 
-				List<Device> avPlayers = avPlayerDeviceManager.getDevices();
-				AVPlayer avPlayer = (AVPlayer) avPlayers.get(0);
-
-				avPlayerDeviceManager.setSelectedDevice(avPlayer);
-
 				avPlayerDeviceManager.execute(new DeviceCommand<AVPlayer>() {
 					@Override
 					public void execute(AVPlayer selectedDevice) {
@@ -197,11 +320,21 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	}
 
 	private void refreshDeviceList() {
-		List<Device> devices = tvDeviceManager.getDevices();
-		DeviceAdapter deviceAdapter = new DeviceAdapter(this, devices);
+		List<Device> tvControllers = tvControllerDeviceManager.getDevices();
+		List<Device> avPlayers = avPlayerDeviceManager.getDevices();
+		List<Device> imageViewers = imageViewerDeviceManager.getDevices();
 
-		listViewDevices.setAdapter(deviceAdapter);
-		tvDeviceManager.setSelectedDevice(null);
+		DeviceAdapter tvControllerAdapter = new DeviceAdapter(this, tvControllers);
+		DeviceAdapter avPlayerAdapter = new DeviceAdapter(this, avPlayers);
+		DeviceAdapter imageViewerAdapter = new DeviceAdapter(this, imageViewers);
+
+		listViewTVControllers.setAdapter(tvControllerAdapter);
+		listViewAVPlayers.setAdapter(avPlayerAdapter);
+		listViewImageViewers.setAdapter(imageViewerAdapter);
+
+		tvControllerDeviceManager.setSelectedDevice(null);
+		avPlayerDeviceManager.setSelectedDevice(null);
+		imageViewerDeviceManager.setSelectedDevice(null);
 	}
 
 	private void refreshSongList() {
@@ -213,53 +346,6 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 		// mediaFinder.setSelectedSong(null);
 	}
 
-	@Override
-	public void createdServiceProvider() {
-		tvDeviceManager = new DeviceManager<TVController>(DeviceType.DEVICE_TV_CONTROLLER, serviceProviderManager);
-		tvDeviceManager.addObserver(this);
-
-		avPlayerDeviceManager = new DeviceManager<AVPlayer>(DeviceType.DEVICE_AVPLAYER, serviceProviderManager);
-		// TODO: Fix implement same interface multiple times!!!
-		// avPlayerDeviceManager.addObserver(this);
-
-		initializeTabTouch();
-
-		refreshDeviceList();
-	}
-
-	@Override
-	public void changedSelectedDevice(TVController selectedDevice) {
-		if (selectedDevice == null) {
-			textViewSelectedDevice.setText("No TV-controller selected.");
-
-			tabHostMain.setCurrentTab(0);
-
-			tabRemote.setEnabled(false);
-			// tabMedia.setEnabled(false);
-		} else {
-			textViewSelectedDevice.setText("Selected TV-controller: " + selectedDevice.getName());
-
-			tabRemote.setEnabled(true);
-
-			// TODO: Add AVPlayers in the devicesList
-			tabMedia.setEnabled(true);
-		}
-	}
-
-	@Override
-	public void addedDevice(TVController device) {
-		DeviceAdapter arrayAdapter = (DeviceAdapter) listViewDevices.getAdapter();
-
-		arrayAdapter.add(device);
-	}
-
-	@Override
-	public void removedDevice(TVController device) {
-		DeviceAdapter arrayAdapter = (DeviceAdapter) listViewDevices.getAdapter();
-
-		arrayAdapter.remove(device);
-	}
-
 	// TODO: Add button to disconnect from the selected device
 
 	public void refreshDeviceListEvent(View view) {
@@ -267,7 +353,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	}
 
 	private void sendRemoteKey(final RemoteKey remoteKey) {
-		tvDeviceManager.execute(new DeviceCommand<TVController>() {
+		tvControllerDeviceManager.execute(new DeviceCommand<TVController>() {
 			@Override
 			public void execute(TVController tvController) {
 				tvController.sendRemoteKey(remoteKey);
@@ -356,7 +442,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	}
 
 	public void sendTouchClickEvent(View view) {
-		tvDeviceManager.execute(new DeviceCommand<TVController>() {
+		tvControllerDeviceManager.execute(new DeviceCommand<TVController>() {
 			@Override
 			public void execute(TVController tvController) {
 				tvController.sendTouchClick();
@@ -367,7 +453,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	// TODO: Add more buttons for numeric keys/dash, play-keys, color-keys, ..
 
 	public void openWebPageEvent(View view) {
-		tvDeviceManager.execute(new DeviceCommand<TVController>() {
+		tvControllerDeviceManager.execute(new DeviceCommand<TVController>() {
 			@Override
 			public void execute(TVController tvController) {
 				String URL = editTextBrowseTerm.getText().toString();
@@ -378,7 +464,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	}
 
 	public void searchInternetEvent(View view) {
-		tvDeviceManager.execute(new DeviceCommand<TVController>() {
+		tvControllerDeviceManager.execute(new DeviceCommand<TVController>() {
 			@Override
 			public void execute(TVController tvController) {
 				String searchTerm = editTextBrowseTerm.getText().toString();
@@ -389,7 +475,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	}
 
 	public void sendKeyboardStringEvent(View view) {
-		tvDeviceManager.execute(new DeviceCommand<TVController>() {
+		tvControllerDeviceManager.execute(new DeviceCommand<TVController>() {
 			@Override
 			public void execute(TVController tvController) {
 				String text = editTextBrowseTerm.getText().toString();
@@ -401,7 +487,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	}
 
 	public void closeWebPageEvent(View view) {
-		tvDeviceManager.execute(new DeviceCommand<TVController>() {
+		tvControllerDeviceManager.execute(new DeviceCommand<TVController>() {
 			@Override
 			public void execute(TVController tvController) {
 				tvController.closeWebPage();
@@ -410,7 +496,7 @@ public class Main extends Activity implements ServiceProviderObserver, DeviceObs
 	}
 
 	public void goHomePageEvent(View view) {
-		tvDeviceManager.execute(new DeviceCommand<TVController>() {
+		tvControllerDeviceManager.execute(new DeviceCommand<TVController>() {
 			@Override
 			public void execute(TVController tvController) {
 				tvController.goHomePage();
